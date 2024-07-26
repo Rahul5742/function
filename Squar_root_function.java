bd4c0f455e0351ee11065beef5361d09bd9d749e
Rahul5742 <rkgamingchannel8@gmail.com>
2024-07-26 09:08:30 +0545

    Squar root function
    =Math.sqrt();

---
+++ Squar_root_function.java
@@ -0,0 +1,7 @@
+public class Squar_root_function {
+    public static void main(String[] args) {
+        double a = 25;
+        double b = Math.sqrt(a);
+        System.out.println("This is squar root fo a = "+b);
+    }
+}
